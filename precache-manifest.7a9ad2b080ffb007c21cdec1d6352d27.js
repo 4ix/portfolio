ca6d66d246c63aaf736d8cd7bd48d39df6283c6f
john <jinjihoon@gmail.com>
2020-09-22 23:28:50 +0900

    Updates

---
+++ precache-manifest.7a9ad2b080ffb007c21cdec1d6352d27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7713537a75f87ba4bdd3b7af43505b25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd17805bf72999606e8",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f6bd3ea04b393b202b56",
+    "url": "/static/css/main.fffb7e15.chunk.css"
+  },
+  {
+    "revision": "5fd17805bf72999606e8",
+    "url": "/static/js/2.38cf3e11.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.38cf3e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6bd3ea04b393b202b56",
+    "url": "/static/js/main.4f3312ac.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
